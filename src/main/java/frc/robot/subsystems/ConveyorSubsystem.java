d7ebcc51c28875a5e08f8922ef0cf518acebd2e5
Pm6grmV1 <jrufusblair@gmail.com>
2024-02-10 14:04:01 -0600

    Newly made conveyor Sunsystem & Commands

---
+++ src/main/java/frc/robot/subsystems/ConveyorSubsystem.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+public class ConveyorSubsystem extends SubsystemBase {
+  /** Creates a new ConveryorSubsystem. */
+  private CANSparkFlex conveyorMotor = new CANSparkFlex(37, MotorType.kBrushless);
+
+  public ConveyorSubsystem() {}
+
+  public void conveyorTurn(double conveyorSpeed) {
+    conveyorMotor.set(conveyorSpeed);
+  }  
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}

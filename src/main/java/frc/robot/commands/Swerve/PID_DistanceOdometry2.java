eae7e6c532d5fa3687fb99042b9b976580a2d50d
KelliB2 <77307653+KelliB2@users.noreply.github.com>
2024-03-01 16:16:44 -0600

    updated the flip check for red autos

--- src/main/java/frc/robot/commands/Swerve/PID_DistanceOdometry2.java
+++ src/main/java/frc/robot/commands/Swerve/PID_DistanceOdometry2.java
@@ -7,8 +7,11 @@ package frc.robot.commands.Swerve;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
+import frc.robot.GlobalVariables;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class PID_DistanceOdometry2 extends Command {
@@ -86,10 +89,11 @@ public class PID_DistanceOdometry2 extends Command {
 			isFinished();
 		}
 
-		// if (GlobalVariables.alliance == Alliance.Red){
-		// 	X_Output = -X_Output;
-		// 	Y_Output = -Y_Output;
-		// }
+		 if (DriverStation.getAlliance().get() == Alliance.Red){
+		 	X_Output = -X_Output;
+		 	Y_Output = -Y_Output;
+			Yaw_Output = -Yaw_Output;
+		 }
 
 		translation = new Translation2d(X_Output, Y_Output).times(Constants.Swerve.maxSpeed);
 		rotation = Yaw_Output;

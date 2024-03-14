6a45c5bedd3795dbbf5625c9ac3e2e36cd58418d
JPSeidlitz <seidfam3@gmail.com>
2024-03-14 10:00:28 -0500

    added flash for note and shooter velo leds

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -4,14 +4,21 @@
 
 package frc.robot.subsystems;
 
+import javax.swing.plaf.basic.BasicTreeUI.TreeCancelEditingAction;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDs extends SubsystemBase {
   private AddressableLED ledstrip;
   private AddressableLEDBuffer ledBuffer;
+  boolean ledInit = false;
+  double initTime = 0;
+  double currTime = 0;
+  double flashCycleTime = .1;
 
   public LEDs() {
     ledstrip = new AddressableLED(9);
@@ -28,6 +35,12 @@ public class LEDs extends SubsystemBase {
     }
     ledstrip.setData(ledBuffer);
   }
+  public void setRed(int offFromMax){
+    for (var i = 0; i < ledBuffer.getLength() - offFromMax; i++) {
+      ledBuffer.setRGB(i, 255, 0, 0);
+    }
+    ledstrip.setData(ledBuffer);
+  }
 
   public void setGreen(){
     for (var i = 0; i < ledBuffer.getLength(); i++) {
@@ -35,6 +48,13 @@ public class LEDs extends SubsystemBase {
     }
     ledstrip.setData(ledBuffer);
   }
+  public void setGreen(int offFromMax){
+    for (var i = 0; i < ledBuffer.getLength() - offFromMax; i++) {
+      ledBuffer.setRGB(i, 0, 255, 0);
+    }
+    ledstrip.setData(ledBuffer);
+  }
+  
 
   public void setBlue(){
     for (var i = 0; i < ledBuffer.getLength(); i++) {
@@ -49,13 +69,60 @@ public class LEDs extends SubsystemBase {
     }
     ledstrip.setData(ledBuffer);
   }
+  public void setWhite(int offFromMax){
+    for (var i = 0; i < ledBuffer.getLength() - offFromMax; i++) {
+      ledBuffer.setRGB(i, 100, 100, 100);
+    }
+    ledstrip.setData(ledBuffer);
+  }
 
   @Override
   public void periodic() {
     if (SmartDashboard.getBoolean("beam broken", false)) {
-      setGreen();
+      if (!ledInit) {
+        ledInit = true;
+        initTime = Timer.getFPGATimestamp();
+      }
+      currTime = Timer.getFPGATimestamp() - initTime;
+
+      if (currTime < flashCycleTime) {
+        setGreen(2);
+      } else if (currTime < flashCycleTime * 2) {
+        setWhite(2);
+      }if (currTime < flashCycleTime * 3) {
+        setGreen(2);
+      } else if (currTime < flashCycleTime * 4) {
+        setWhite(2);
+      }if (currTime < flashCycleTime * 5) {
+        setGreen(2);
+      } else if (currTime < flashCycleTime * 6) {
+        setWhite(2);
+      }if (currTime < flashCycleTime * 7) {
+        setGreen(2);
+      } else if (currTime < flashCycleTime * 8) {
+        setWhite(2);
+      } if (currTime < flashCycleTime * 9) {
+        setGreen(2);
+      } else if (currTime < flashCycleTime * 10) {
+        setWhite(2);
+      } else {
+        setGreen(2);
+      }
+      
     } else {
-      setRed();
+      setRed(2);
+    }
+
+    if (Math.abs(6500 - SmartDashboard.getNumber("bottomVelo", currTime)) < 100) {
+      ledBuffer.setRGB(ledBuffer.getLength(), 0, 0, 100);
+    } else {
+      ledBuffer.setRGB(ledBuffer.getLength(), 100, 100, 0);
+    }
+
+    if (Math.abs(6500 - SmartDashboard.getNumber("topVelo", currTime)) < 100) {
+      ledBuffer.setRGB(ledBuffer.getLength() - 1, 0, 0, 100);
+    } else {
+      ledBuffer.setRGB(ledBuffer.getLength() - 1, 100, 100, 100);
     }
   }
 }
